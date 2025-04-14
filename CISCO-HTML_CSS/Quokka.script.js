e21d7d9f9540b850985a90f9fe42b13d91d070f7
CYNTHIA-DAMIAN <syntearahmed@gmail.com>
2025-04-14 10:10:00 +0100

    My first commit to my personal study space

---
+++ CISCO-HTML_CSS/Quokka.script.js
@@ -0,0 +1,36 @@
+// /* Task 1: Write a JavaScript function that takes a string as input and returns the string with all vowels removed.
+// Example input: "hello world"
+// Expected output: "hll wrld"
+// */
+// let myResult = document.querySelector('#result');
+// const myTest = document.querySelector('#myTest');
+// let myStrings = "Thank You My Meta For accepting being my Tutor";
+// function vowelRemover(theString){
+//     return theString.replace(/[aeiouAEIOU]/g, "");
+// }
+//  myResult.textContent = vowelRemover(myStrings);
+// console.log(myResult);
+
+// //Create a function called doubleAndTriple that takes a single argument num and returns an object with two properties: double and triple. The double property should contain a function that returns the double of num, and the triple property should contain a function that returns the triple of num.
+// function doubleAndTriple(num){
+//     const myObject = {
+//         double: function () { 
+//             return num * 2; 
+//         },
+//         triple: function () { 
+//             return num * 3;
+//          }
+//         }
+//         return myObject;    
+       
+// }
+// const result = doubleAndTriple(5);
+// myTest.innerHTML = `Double: ${result.double()}<br>Triple: ${result.triple()}`;
+// console.log(doubleAndTriple(5).double() ); 
+ 
+
+// // myTest.innerHTML = Double: ${result.double()}<br>Triple: ${result.triple()};
+
+
+
+    
